de5d33d3ff772575cac0cce3afa18d64a36289fc
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-19 18:08:05 +0100

    feat: create getJwt method

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,9 @@
 import http from './httpService';
+import { getFromStorage, tokenKey } from 'utils/index';
 
 const apiEndpoint = '/auth';
 
 export const login = (credentials) =>
   http.post(`${apiEndpoint}/login`, credentials);
-  
+
+export const getJwt = () => getFromStorage(tokenKey)?.token;
