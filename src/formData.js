1f974608a7f6125956bc4ca5d37ffa654ac72379
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-19 17:53:00 +0100

    feat: add new properties

--- src/formData.js
+++ src/formData.js
@@ -1,3 +1,44 @@
+export const loginInputs = [
+  {
+    type: 'text',
+    id: 'username',
+    placeholder: 'username',
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'password',
+  },
+];
+
+export const registerInputs = [
+  {
+    type: 'text',
+    id: 'name',
+    placeholder: 'name',
+  },
+  {
+    type: 'text',
+    id: 'username',
+    placeholder: 'username',
+  },
+  {
+    type: 'email',
+    id: 'email',
+    placeholder: 'email',
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'password',
+  },
+  {
+    type: 'password',
+    id: 'confirmPassword',
+    placeholder: 'confirm password',
+  },
+];
+
 export const userInputs = [
   {
     id: 'username',
