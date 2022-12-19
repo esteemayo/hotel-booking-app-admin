f7f1e41cf6b4404d9038f0854f9e09b95a1ea470
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-19 17:54:33 +0100

    feat: create httpService for axios

---
+++ src/services/httpService.js
@@ -0,0 +1,46 @@
+import Axios from 'axios';
+import { toast } from 'react-toastify';
+
+import logger from './logService';
+
+const devEnv = process.env.NODE_ENV !=='production';
+const { REACT_APP_DEV_API_URL,REACT_APP_PROD_API_URL } = process.env;
+
+const API = Axios.create({
+  baseURL: `${devEnv ? REACT_APP_DEV_API_URL :REACT_APP_PROD_API_URL}`,
+  headers:{
+    Accept: 'application/json',
+  },
+});
+
+API.interceptors.request.use((req) => {
+  req.headers.common['Authorization'] = `Bearer `;
+  return req;
+},(error) => {
+  logger.log(error);
+  return Promise.reject(error);
+});
+
+API.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error('An unexpected error occurred');
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: API.get,
+  post: API.post,
+  patch: API.patch,
+  delete: API.delete,
+};
+
+export default http;
