093bf4191ae512d8128ed05c34a1edeecadbb00e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-19 17:53:32 +0100

    feat: change file extension

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { login } from 'services/authService';
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
+import './login.scss';
+
+const initialState = {
+  username: '',
+  password: '',
+};
+
+const Login = ({ inputs }) => {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState(initialState);
+  const { user, error, loading, loginFailure, loginStart, loginSuccess }
+    = useGlobalAuthContext();
+
+  const handleChange = ({ target: input }) => {
+    const { id, value } = input;
+    setCredentials((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await handleLogin();
+    user && await navigate('/');
+  };
+
+  const handleLogin = async () => {
+    loginStart();
+    try {
+      const { data } = await login({ ...credentials });
+      loginSuccess(data);
+    } catch (err) {
+      loginFailure(err.response.data);
+    }
+  };
+
+  return (
+    <div className='login'>
+      <div className='login__container'>
+        {inputs.map((input) => {
+          const { id, type, placeholder } = input;
+          return (
+            <input
+              id={id}
+              key={id}
+              type={type}
+              placeholder={placeholder}
+              onChange={handleChange}
+              className='login__input'
+            />
+          );
+        })}
+        <button
+          type='submit'
+          disabled={loading}
+          onClick={handleSubmit}
+          className='login__button'
+        >
+          Login
+        </button>
+        {error && <span className='login__error'>{error.message}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
