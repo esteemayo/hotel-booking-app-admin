f9d311dbf314c7282ff59dc70058a659232020ef
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-19 17:55:35 +0100

    feat: add utils/index

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+export const tokenKey = 'access_token';
+
+export const getFromStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+export const setToStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const clearFromStorage = () => {
+  return localStorage.clear();
+};
