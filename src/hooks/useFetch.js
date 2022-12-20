ad459d451dba69856e5a288515633be24076e195
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-20 20:15:05 +0100

    feat: create useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import http from 'services/httpService';
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    url && (async () => {
+      setLoading(true);
+      try {
+        const { data } = await http.get(url);
+        setData(data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    })();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const { data } = await http.get(url);
+      setData(data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return {
+    data,
+    loading,
+    error,
+    reFetch,
+  };
+};
+
+export default useFetch;
