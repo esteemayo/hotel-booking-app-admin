6f777d3291d430ee30a8a55ff80eb344117b975c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-21 01:13:50 +0100

    feat: add new api endpoint

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,4 +2,8 @@ import http from './httpService';
 
 const apiEndpoint = '/users';
 
+const userUrl = (userId) => `${apiEndpoint}/${userId}`;
+
 export const getUsers = () => http.get(apiEndpoint);
+
+export const deleteUser = (userId) => http.delete(userUrl(userId));
