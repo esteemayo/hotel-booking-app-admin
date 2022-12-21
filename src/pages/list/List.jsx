807bb0c77ab3c91307b17752c4d7874dc051cdb2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-21 02:22:49 +0100

    feat: modify

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,5 +1,3 @@
-import { useLocation } from 'react-router-dom';
-
 import Navbar from 'components/navbar/Navbar';
 import Sidebar from 'components/sidebar/Sidebar';
 import DataTable from 'components/datatable/DataTable';
@@ -7,15 +5,12 @@ import DataTable from 'components/datatable/DataTable';
 import './list.scss';
 
 const List = ({ columns }) => {
-  const { pathname } = useLocation();
-  const path = pathname.split('/')[1];
-
   return (
     <div className='list'>
       <Sidebar />
       <div className='list-container'>
         <Navbar />
-        <DataTable path={path} columns={columns} />
+        <DataTable columns={columns} />
       </div>
     </div>
   );
