32a0848febfaf8b98888228eb15f0ce19827daba
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-21 15:37:00 +0100

    feat: import NewHotel

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,19 @@ import Layout from './Layout/Layout';
 import List from './list/List';
 import Login from './login/Login';
 import New from './new/New';
+import NewHotel from './newHotel/NewHotel';
 import NotFound from './notFound/NotFound';
 import SharedLayout from './Layout/SharedLayout';
 import Single from './single/Single';
 
-export { Home, Layout, List, Login, New, NotFound, SharedLayout, Single };
+export {
+  Home,
+  Layout,
+  List,
+  Login,
+  New,
+  NewHotel,
+  NotFound,
+  SharedLayout,
+  Single,
+};
