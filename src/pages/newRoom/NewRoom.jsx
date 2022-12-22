0ccf7e6f9e242a4dde4ac77484a8840d94036412
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-22 02:28:26 +0100

    feat: modify

--- src/pages/newRoom/NewRoom.jsx
+++ src/pages/newRoom/NewRoom.jsx
@@ -4,6 +4,7 @@ import { roomInputs } from 'formData';
 import useFetch from 'hooks/useFetch';
 import Navbar from 'components/navbar/Navbar';
 import Sidebar from 'components/sidebar/Sidebar';
+import { createRoom } from 'services/roomService';
 
 import './newRoom.scss';
 import { createRoom } from 'services/roomService';
