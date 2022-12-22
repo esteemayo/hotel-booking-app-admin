93f617d4a54e7ab8b2535466bbd308b407b1b7b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-22 18:33:51 +0100

    feat: add a new user api endpoint

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,6 +8,8 @@ export const getUsers = () => http.get(apiEndpoint);
 
 export const getUser = (userId) => http.get(userUrl(userId));
 
+export const getUserStats = () => http.get(`${apiEndpoint}/stats`);
+
 export const createUser = (credentials) =>
   http.post(`${apiEndpoint}/register`, credentials);
 
