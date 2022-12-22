3ed750fb300569502431b6874ccef4cb1351124b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-22 02:26:49 +0100

    feat: format

--- src/services/roomService.js
+++ src/services/roomService.js
@@ -8,6 +8,7 @@ export const getRooms = () => http.get(apiEndpoint);
 
 export const getRoom = (roomId) => http.get(roomUrl(roomId));
 
-export const createRoom = (hotelId, room) => http.post(`${apiEndpoint}/${hotelId}`, room);
+export const createRoom = (hotelId, room) =>
+  http.post(`${apiEndpoint}/${hotelId}`, room);
 
 export const deleteRoom = (roomId) => http.delete(roomUrl(roomId));
