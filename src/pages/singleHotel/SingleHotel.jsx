3d1c8aa430c8f1204795b0f20ea15ee103a658ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-23 01:55:21 +0100

    feat: rename file

---
+++ src/pages/singleHotel/SingleHotel.jsx
@@ -0,0 +1,71 @@
+import { useLocation } from 'react-router-dom';
+
+import { data } from 'data';
+import List from 'components/table/Table';
+import Chart from 'components/chart/Chart';
+import Navbar from 'components/navbar/Navbar';
+import Sidebar from 'components/sidebar/Sidebar';
+
+import './singleHotel.scss';
+
+const SingleHotel = () => {
+  const { state } = useLocation();
+  console.log(state)
+
+  return (
+    <div className='single'>
+      <Sidebar />
+      <div className='single-container'>
+        <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <div className='edit-button'>Edit</div>
+            <h1 className='title'>Information</h1>
+            <div className='item'>
+              <img
+                src='https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+                alt=''
+                className='item-img'
+              />
+              <div className='details'>
+                <h1 className='item-title'>Jane Doe</h1>
+                <div className='detail-item'>
+                  <span className='item-key'>Email:</span>
+                  <span className='item-value'>janedoe@gmail.com</span>
+                </div>
+                <div className='detail-item'>
+                  <span className='item-key'>Phone:</span>
+                  <span className='item-value'>+1 2345 67 89</span>
+                </div>
+                <div className='detail-item'>
+                  <span className='item-key'>Address:</span>
+                  <span className='item-value'>
+                    Elton St. 234 Garden Yd. NewYork
+                  </span>
+                </div>
+                <div className='detail-item'>
+                  <span className='item-key'>Country:</span>
+                  <span className='item-value'>USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='right'>
+            <Chart
+              data={data}
+              dataKey='Total'
+              aspect={3 / 1}
+              title='User Spending (Last 6 Months)'
+            />
+          </div>
+        </div>
+        <div className='bottom'>
+          <h1 className='title'>Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleHotel;
