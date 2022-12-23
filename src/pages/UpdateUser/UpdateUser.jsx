ac4f70de0ecfd4ae41a4f850848958ab25a42830
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-23 22:19:04 +0100

    feat: import useEffcet hook

--- src/pages/UpdateUser/UpdateUser.jsx
+++ src/pages/UpdateUser/UpdateUser.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { DriveFolderUploadOutlined } from '@mui/icons-material';
 
