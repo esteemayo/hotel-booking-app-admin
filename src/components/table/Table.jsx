63873741050e85bf8b2f1043cd7caeec68779563
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-24 00:23:27 +0100

    feat: modify

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,7 +1,7 @@
 import useFetch from 'hooks/useFetch';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
+import Tableuser from '@mui/material/Tableuser';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
@@ -16,7 +16,7 @@ const List = () => {
     <TableContainer component={Paper} className='table'>
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
-          <TableRow>
+          <Tableuser>
             <TableCell className='table-cell' style={{ fontSize: '1.6rem' }}>
               User ID
             </TableCell>
@@ -41,43 +41,43 @@ const List = () => {
             <TableCell className='table-cell' style={{ fontSize: '1.6rem' }}>
               Status
             </TableCell>
-          </TableRow>
+          </Tableuser>
         </TableHead>
         <TableBody>
-          {data?.users?.map((row) => (
-            <TableRow key={row.id}>
+          {data?.users?.map((user) => (
+            <Tableuser key={user.id}>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row._id}
+                {user._id}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
                 <div className='cell-wrapper'>
                   <img
-                    src={row.img}
-                    alt={row.username ?? 'https://i.ibb.co/MBtjqXQ/no-avatar.gif'}
+                    src={user.img}
+                    alt={user.username ?? 'https://i.ibb.co/MBtjqXQ/no-avatar.gif'}
                     className='image'
                   />
-                  {row.name}
+                  {user.name}
                 </div>
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row.username}
+                {user.username}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row.createdAt}
+                {user.createdAt}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row.country}
+                {user.country}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row.role}
+                {user.role}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                {row.email}
+                {user.email}
               </TableCell>
               <TableCell className='table-cell' style={{ fontSize: '1.3rem' }}>
-                <span className={`status`}>{row.active === true ? 'Active' : 'Inactive'}</span>
+                <span className={`status`}>{user.active === true ? 'Active' : 'Inactive'}</span>
               </TableCell>
-            </TableRow>
+            </Tableuser>
           ))}
         </TableBody>
       </Table>
