4e991662a4ab85656385a3efb84c55f37e7e0d36
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-25 15:40:53 +0100

    feat: modify

--- src/pages/singleRoom/SingleRoom.jsx
+++ src/pages/singleRoom/SingleRoom.jsx
@@ -28,7 +28,7 @@ const SingleRoom = () => {
                   <span className='item-value'>{state.price}</span>
                 </div>
                 <div className='detail-item'>
-                  <span className='item-key'>Max people:</span>
+                  <span className='item-key'>Max People:</span>
                   <span className='item-value'>{state.maxPeople}</span>
                 </div>
                 <div className='detail-item'>
