7c1792104a03564eec2a12a09061ef78ab92d8b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-25 13:33:05 +0100

    feat: add a new hotel api endpoint

--- src/services/hotelService.js
+++ src/services/hotelService.js
@@ -8,6 +8,9 @@ export const getHotels = () => http.get(apiEndpoint);
 
 export const getHotel = (hotelId) => http.get(hotelUrl(hotelId));
 
+export const getHotelBySlug = (slug) =>
+  http.get(`${apiEndpoint}/details/${slug}`);
+
 export const createHotel = (hotel) => http.post(apiEndpoint, hotel);
 
 export const updateHotel = (hotelId, hotel) =>
