085925ad3559a4ad50feb3501d3d3bf9ad6e277b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-25 15:31:15 +0100

    feat: modify

--- src/pages/singleRoom/SingleRoom.jsx
+++ src/pages/singleRoom/SingleRoom.jsx
@@ -21,11 +21,6 @@ const SingleRoom = () => {
             </div>
             <h1 className='title'>Information</h1>
             <div className='item'>
-              <img
-                src='https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-                alt=''
-                className='item-img'
-              />
               <div className='details'>
                 <h1 className='item-title'>Jane Doe</h1>
                 <div className='detail-item'>
