f1082187ee5e21957d9ea259014bb45cce1ea410
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-31 00:48:14 +0100

    fix: modify

--- src/context/darkmode/DarkModeReducer.js
+++ src/context/darkmode/DarkModeReducer.js
@@ -1,28 +1,28 @@
 import { DARK, LIGHT, TOGGLE } from './DarkModeTypes';
 
 const DarkModeReducer = (state, { payload, type }) => {
-  if (type === LIGHT) {
-    return {
-      ...state,
-      darkMode: payload,
-    };
-  }
+  switch (type) {
+    case LIGHT:
+      return {
+        ...state,
+        darkMode: payload,
+      };
 
-  if (type === DARK) {
-    return {
-      ...state,
-      darkMode: payload,
-    };
-  }
+    case DARK:
+      return {
+        ...state,
+        darkMode: payload,
+      };
 
-  if (type === TOGGLE) {
-    return {
-      ...state,
-      darkMode: payload,
-    };
-  }
+    case TOGGLE:
+      return {
+        ...state,
+        darkMode: payload,
+      };
 
-  throw new Error('No matching action type');
+    default:
+      return state;
+  };
 };
 
 export default DarkModeReducer;
