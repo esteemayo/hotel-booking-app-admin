fef44b665e4e7255885a928cc7cc2ed72193032b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-30 15:47:34 +0100

    feat: import useEffect hook

--- src/pages/singleUser/SingleUser.jsx
+++ src/pages/singleUser/SingleUser.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { data } from 'data';
