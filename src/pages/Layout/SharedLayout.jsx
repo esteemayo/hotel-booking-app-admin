c9947afa8a3872f5caeb1488931b414dacb5e915
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-01 00:59:55 +0100

    fix: add toast container

--- src/pages/Layout/SharedLayout.jsx
+++ src/pages/Layout/SharedLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 const SharedLayout = () => {
   return (
     <>
+      <ToastContainer />
       <Outlet />
     </>
   );
