74cecffc06d1a3a04273b83c45ff1ac3781086e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-23 00:00:54 +0100

    fix: modify

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,14 +1,9 @@
-import Navbar from 'components/navbar/Navbar';
-import Sidebar from 'components/sidebar/Sidebar';
-
 import './notFound.scss';
 
 const NotFound = () => {
   return (
     <div className='notfound'>
-      <Sidebar />
       <div className='notfound-container'>
-        <Navbar />
         <div className='img-wrapper'>
           <img src='assets/img/404.png' alt='' />
         </div>
